a928bfaacefbca69c6e16d8f01caa8fa47768a85
karthishaveeramachaneni <145866760+Karthisha25@users.noreply.github.com>
2025-09-24 22:42:37 +0530

    Time: 0 ms (100%), Space: 17.8 MB (43.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,9 +1,5 @@
-from typing import List
-
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        combined = nums1[:m] + nums2
-        combined.sort()
-        nums1[:len(combined)] = combined
-        for i in range(len(combined), m + n):
-            nums1[i] = 0
+        nums3 = nums1[:m] + nums2  
+        nums3.sort()  
+        nums1[:] = nums3  
