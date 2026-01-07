228c795d8d8b53147fb3a12710232d6a33a5e610
karthishaveeramachaneni <145866760+Karthisha25@users.noreply.github.com>
2026-01-07 17:22:18 +0530

    Time: 4 ms (34.67%), Space: 72.1 MB (8.33%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,29 +1,25 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
-        int n = arr.length;
-        int l = 0;
-        int r = k - 1;
-        int sum = 0;
-        int cnt = 0;
-        int target = threshold * k;
-        for (int i = 0; i < k; i++) {
-            sum += arr[i];
+        int n= arr.length;
+        int l=0;
+        int r=k-1;
+        int sum=0;
+        int cnt=0;
+        for(int i=0; i<k; i++){
+            sum+= arr[i];
         }
-
-        if (sum >= target) {
+        if((double)sum/k>= threshold){
             cnt++;
         }
-        while (r < n - 1) {
-            sum += arr[r + 1];
-            sum -= arr[l];
-            l++;
+        while(r<n-1){
+            sum+=arr[r+1];
+            sum-=arr[l];
+            l++; 
             r++;
-
-            if (sum >= target) {
+            if((double)sum/k>= threshold){
                 cnt++;
             }
         }
-
         return cnt;
     }
 }
