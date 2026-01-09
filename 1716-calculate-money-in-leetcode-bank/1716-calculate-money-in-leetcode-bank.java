075ef99364ec33b8ea6a8e83ad5b579095012cee
karthishaveeramachaneni <145866760+Karthisha25@users.noreply.github.com>
2026-01-09 20:18:20 +0530

    Time: 1 ms (55.2%), Space: 42.1 MB (64%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -2,16 +2,13 @@ class Solution {
     public int totalMoney(int n) {
         int total = 0;
         int weekStart = 1;
-
         for(int i = 1; i <= n; i++) {
             total += weekStart;
             weekStart++;
-
             if(i % 7 == 0) {
                 weekStart = (i / 7) + 1;
             }
         }
-
         return total;
     }
 }
