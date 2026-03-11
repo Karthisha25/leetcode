60e41e37da1472857454ae6c99eeec7ec1f85b30
karthishaveeramachaneni <145866760+Karthisha25@users.noreply.github.com>
2026-03-11 18:41:21 +0530

    Time: 33 ms (23.02%), Space: 46.8 MB (25.17%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,54 +1,41 @@
-import java.util.*;
-
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-
-        if(s1.length() > s2.length()) return false;
-
-        HashMap<Character,Integer> map1 = new HashMap<>();
-        HashMap<Character,Integer> map2 = new HashMap<>();
-
-        // build frequency map for s1
-        for(int i = 0; i < s1.length(); i++){
-            char c = s1.charAt(i);
-
+        if(s1.length()> s2.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map1= new HashMap<>();
+        HashMap<Character, Integer> map2= new HashMap<>();
+        for(int i=0; i<s1.length();i++){
+            char c= s1.charAt(i);
             if(map1.containsKey(c)){
-                map1.put(c, map1.get(c) + 1);
-            }else{
+                map1.put(c, map1.get(c)+1);
+            }
+            else{
                 map1.put(c, 1);
             }
         }
-
-        int l = 0;
-
-        for(int r = 0; r < s2.length(); r++){
-
-            char c = s2.charAt(r);
-
+        int l=0;
+        for(int r=0; r<s2.length(); r++){
+            char c= s2.charAt(r);
             if(map2.containsKey(c)){
-                map2.put(c, map2.get(c) + 1);
-            }else{
-                map2.put(c, 1);
+                map2.put(c, map2.get(c)+1);
             }
-
-            // if window size exceeds s1 length
-            if(r - l + 1 > s1.length()){
-
-                char left = s2.charAt(l);
-                map2.put(left, map2.get(left) - 1);
-
-                if(map2.get(left) == 0){
+            else{
+                map2.put(c,1);
+            }
+            if(r-l+1>s1.length()){
+                char left=s2.charAt(l);
+                map2.put(left, map2.get(left)-1);
+                if(map2.get(left)==0){
                     map2.remove(left);
                 }
-
                 l++;
             }
-
             if(map1.equals(map2)){
                 return true;
             }
-        }
 
+        }
         return false;
     }
 }
