31365bb48ead566f720e37a4e31c1a8801b63a9d
karthishaveeramachaneni <145866760+Karthisha25@users.noreply.github.com>
2026-05-10 10:55:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        ArrayList<Integer>l=new ArrayList<>();
-        for(int i=0; i<n; i++){
-            if(nums[i]!=0){
-                l.add(nums[i]);
+        int j=-1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                j=i;
+                break;
             }
         }
-        for(int i=0; i<l.size(); i++){
-            nums[i]=l.get(i);
-        }
-        for(int i=l.size(); i<n; i++){
-            nums[i]=0;
+        for(int i=j+1; i<nums.length; i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
         }
     }
 }
