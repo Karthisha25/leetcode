c9249d47072e43dea0e776ec7749082af106dde1
karthishaveeramachaneni <145866760+Karthisha25@users.noreply.github.com>
2026-05-21 11:58:47 +0530

    Time: 0 ms (100%), Space: 48.2 MB (83.24%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -7,11 +7,11 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            else if(target>nums[mid]){
-                low=mid+1;
+            else if(nums[mid] >target){
+                high=mid-1;
             }
             else{
-                high=mid-1;
+                low=mid+1;
             }
         }
         return -1;
